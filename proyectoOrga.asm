e04ef66a797f85bb45152ba8d00c3f08b93707d1
Leonardo Gonzalez <59893051+Leopgf@users.noreply.github.com>
2020-11-22 15:27:40 -0400

    Esqueleto principal

--- proyectoOrga.asm
+++ proyectoOrga.asm
@@ -2,56 +2,70 @@
 
  mensaje1: 	.asciiz "Ingrese el primer numero "
  mensaje2: 	.asciiz "Ingrese el segundo numero "
+ mensaje3:	.asciiz "Indique la operacion a realizar Sumar(0), Restar(1), Multiplicar(2)"
  mensajeFinal: 	.asciiz "El resultado de la operacion es "
  salto:		.asciiz "\n"
- numero1: 	.space 50
- numero2: 	.space 50
+ numero1: 	.space 56
+ numero2: 	.space 56
  
 
 	.text
 
 	li $v0, 4
-	la $a0, mensaje1
+	la $a0, mensaje1 #Mensaje para ingresar el primer numero 
 	syscall 
 	
 	li $v0,8
-	la $a0, numero1 #load byte space into address
-        li $a1, 50 # allot the byte space for string
-        # GUARDADO PARA MAS TARDE save string to t0 move -> $t0,$a0 
+	la $a0, numero1 #Cargar el byte que se va a sobrescribir
+        li $a1, 50 #Se indica máximo de bytes para escribir  
         syscall
         
-	#sw $a0, numero1 #cambiando a sw para guardar el string en memoria
-	
 	li $v0,4
 	la $a0,salto
 	syscall
 	
 	li $v0,4
-	la $a0, mensaje2 #mostrar que tiene que ingresar el segundo numero a operar
+	la $a0, mensaje2 #Mensaje para ingresar segundo número
 	syscall
 	
 	li $v0,8
-	la $a0, numero2 #load byte space into address
-        li $a1, 50 # allot the byte space for string
-        # GUARDADO PARA MAS TARDE save string to t1 move -> move $t1,$a0 
+	la $a0, numero2 # Segundo numero
+        li $a1, 50  
+        syscall
+        
+        li $v0,4
+	la $a0,salto
+	syscall
+        
+        li $v0, 4
+        la $a0, mensaje3 #Mensaje para indicar la operacion
         syscall
 
 	li $v0,4
 	la $a0,salto
 	syscall
+	
+	#El usuario indica la opracion a ralizar
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	beq $t0, 0, suma
+	beq $t0, 1, resta
+	beq $t0, 2, multi
+	
+suma:	li $v0, 1
+	li $a0, 0
+	syscall
 
-	li $v0, 4
-	la $a0, numero1
+resta:	li $v0, 1
+	li $a0, 1
+	syscall
+
+multi:	li $v0, 1
+	li $a0, 2
 	syscall
 	
-	li $v0,4
-	la $a0,salto
-	syscall
-	
-	li $v0, 4
-	la $a0, numero2
-	syscall
-
 	li $v0,10
 	syscall
 
