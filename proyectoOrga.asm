17a79c1828913f7dca3749163f8b60f3b2cc3c98
luismanzano <luisfmanzano@gmail.com>
2020-11-23 19:09:07 -0400

    Se cambio el  y el

--- proyectoOrga.asm
+++ proyectoOrga.asm
@@ -4,7 +4,7 @@
  mensaje2: 	.asciiz "Ingrese el segundo numero "
  mensaje3:	.asciiz "Indique la operacion a realizar Sumar(0), Restar(1), Multiplicar(2)"
  mensajeR: 	.asciiz "El resultado de la operacion es "
- continuar:	.asciiz "\n¿Desea realizar otra operacion?  SI(1), NO(0)\n"
+ continuar:	.asciiz "\nï¿½Desea realizar otra operacion?  SI(1), NO(0)\n"
  salto:		.asciiz "\n"
  numero1: 	.space 50
  numero2: 	.space 50
@@ -30,7 +30,7 @@ regresar: #Si el usuario desea realizar otra operacion
 	
 	li $v0,8
 	la $a0, numero1 #Cargar el byte que se va a sobrescribir
-        li $a1, 50 #Se indica máximo de bytes para escribir  
+        li $a1, 50 #Se indica mï¿½ximo de bytes para escribir  
         syscall
         
 	li $v0,4
@@ -38,7 +38,7 @@ regresar: #Si el usuario desea realizar otra operacion
 	syscall
 	
 	li $v0,4
-	la $a0, mensaje2 #Mensaje para ingresar segundo número
+	la $a0, mensaje2 #Mensaje para ingresar segundo nï¿½mero
 	syscall
 	
 	li $v0,8
@@ -69,8 +69,8 @@ operacion:
 	bgt $t0, 2, operacion
 	
 suma:	
-	li $t0, 49 #Indice para recorrer los numeros ingresados por el usuario
-	li $s0, 0 #Numero de acarreo
+	li $t0, 50 #Indice para recorrer los numeros ingresados por el usuario CAMBIO A 50 AQUI
+	li $t6, 0 #Numero de acarreo
 	#li $t9, 0#Indice para guardar el resultado
 loopS: # Proceso de sumar
 	lb $t1, numero1($t0)
@@ -79,7 +79,7 @@ loopS: # Proceso de sumar
 	lb $t4, tabla($t2)
 	add $s1, $t3,$t4
 	add $s1, $s1, $s0
-	li $s0, 0
+	li $t6, 0
 	bgt $s1, 9, acarreoSuma
 sumaC:	# Se continua con la suma
 	lb $t5,numeros($s1) 	
@@ -111,7 +111,7 @@ final: #Imprimir el resultado de la operacion
 	la $a0,salto
 	syscall
 	
-	li $t0,49
+	li $t0,50 #OTRO CAMBIO DE 49 A 50 AQUI
  #imprimir: 	
 	li $v0, 4
 	la $a0, resultado#($t0)
@@ -135,6 +135,6 @@ final: #Imprimir el resultado de la operacion
 
 acarreoSuma: #Llevo 1 en la suma
 	subi $s1, $s1, 10
-	li $s0, 1
+	li $t6, 1
 	b sumaC
 	
